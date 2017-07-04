713da284b5aae121fd2f0383e9bd0ace52fb4c5a
Nicholas Kelley <nickdk.1995@gmail.com>
2017-07-04 12:00:52 -0400

    rough implementation of 1 off

--- ruby/arrays_and_strings.rb
+++ ruby/arrays_and_strings.rb
@@ -56,4 +56,49 @@ class Arrays_And_Strings
   def palindrome_permutation?(string)
     string.split("").uniq.length == ceil(string.length / 2.0)
   end
+
+  #check if strings are 1 insertion, deletion, or replacement away from being the same
+  def one_away?(a, b)
+    if(abs(a.length - b.length) > 1)
+      false
+    #replacement?
+    elsif(a.length == b.length)
+      if(a == b)
+        true
+      else
+        one_replacement?(a, b)
+      end
+    #insertion or deletion
+    else
+      if(a.length > b.length)
+        one_deletion?(a, b)
+      else
+        one_deletion?(b, 1)
+      end
+    end
+  end
+
+  #based on length check we know if statement will always execute once
+  def one_deletion?(one_delete, verifier)
+    (0..(verifier.length - 1)).each do |i|
+      if(one_delete[i] != verifier[i])
+        return one_delete.insert(i, verifier[i]) == verifier
+      end
+    end
+  end
+
+  def one_replacement?(a, b)
+    replacement? = false
+    (0..(a.length - 1)).each do |i|
+      if(a[i] != b[i])
+        if(replacement?)
+          return false;
+        else
+          replacement? = true
+        end
+      end
+    end
+
+    return true
+  end
 end
